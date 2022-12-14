0aea37a8bbdc9d48bfe5425aaccf337c8e16b740
TomaszPietrzykowski <pietrzykowski77@gmail.com>
2022-12-14 19:22:38 +0100

    custom input component added

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -3,17 +3,12 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { AuthRoutingModule } from './auth-routing.module';
-import { HttpClientModule } from '@angular/common/http';
+import { SharedModule } from '../shared/shared.module';
 import { SigninComponent } from './signin/signin.component';
 import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
   declarations: [SigninComponent, SignupComponent],
-  imports: [
-    CommonModule,
-    AuthRoutingModule,
-    ReactiveFormsModule,
-    HttpClientModule,
-  ],
+  imports: [CommonModule, AuthRoutingModule, ReactiveFormsModule, SharedModule],
 })
 export class AuthModule {}
--- src/app/shared/input/input.component.ts
+++ src/app/shared/input/input.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-input',
   templateUrl: './input.component.html',
-  styleUrls: ['./input.component.scss']
+  styleUrls: ['./input.component.scss'],
 })
 export class InputComponent {
+  @Input() label: string = '';
+  @Input() control!: FormControl;
+  @Input() inputType: string = '';
 
+  showErrors() {
+    const { dirty, touched, errors } = this.control;
+    return dirty && touched && errors;
+  }
 }
