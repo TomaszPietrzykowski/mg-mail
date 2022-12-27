84dc3d179113614b468fa2384c28f02e055915e5
TomaszPietrzykowski <pietrzykowski77@gmail.com>
2022-12-28 00:10:27 +0100

    reply functionality implemented

--- src/app/inbox/email-reply/email-reply.component.ts
+++ src/app/inbox/email-reply/email-reply.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+import { Email } from '../email';
+import { EmailService } from '../email.service';
 
 @Component({
   selector: 'app-email-reply',
   templateUrl: './email-reply.component.html',
-  styleUrls: ['./email-reply.component.scss']
+  styleUrls: ['./email-reply.component.scss'],
 })
 export class EmailReplyComponent {
+  @Input() email!: Email;
 
+  showModal = false;
+
+  constructor(private emailService: EmailService) {}
+
+  ngOnChanges() {
+    const text = this.email.text.replace(/\n/gi, '\n> ');
+
+    this.email = {
+      ...this.email,
+      from: this.email.to,
+      to: this.email.from,
+      subject: `Re: ${this.email.subject}`,
+      text: `\n\n\n------ ${this.email.from} wrote:\n> ${text}`,
+    };
+  }
+
+  onSubmit(email: Email) {
+    this.emailService.sendEmail(email).subscribe(() => {
+      this.showModal = false;
+    });
+  }
 }
