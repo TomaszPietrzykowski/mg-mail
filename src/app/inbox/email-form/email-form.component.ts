7027f6f023df4e1da72d8ecab47837a3925f3802
TomaszPietrzykowski <pietrzykowski77@gmail.com>
2022-12-27 19:24:48 +0100

    email form added

--- src/app/inbox/email-form/email-form.component.ts
+++ src/app/inbox/email-form/email-form.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { Email } from '../email';
 
 @Component({
   selector: 'app-email-form',
   templateUrl: './email-form.component.html',
-  styleUrls: ['./email-form.component.scss']
+  styleUrls: ['./email-form.component.scss'],
 })
 export class EmailFormComponent {
+  @Input() email!: Email;
 
+  emailForm!: FormGroup;
+
+  ngOnInit() {
+    const { subject, from, to, text } = this.email;
+
+    this.emailForm = new FormGroup({
+      subject: new FormControl(subject),
+      from: new FormControl(from),
+      to: new FormControl(to),
+      text: new FormControl(text),
+    });
+  }
 }
